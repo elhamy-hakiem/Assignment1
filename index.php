3536f99cb68fa83d0988b66a59a816d34bb4b71c
Elhamy Abdelhakiem <54361995+elhamy-hakiem@users.noreply.github.com>
2022-06-06 02:01:09 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+//Task1 And Task2 Solution
+$num1 = 10;
+$num2 = 20; 
+
+$num1 =$num1 + $num2;
+
+$num2 = $num1 - 20;
+
+$num1 =$num1 - $num2;
+
+echo "Number1 = ".$num1." : Number2 =  ".$num2 ;
+?>
